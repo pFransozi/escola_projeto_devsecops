30e38252f338fad8a833578394d2798f23271a71
pfransozi <philipehf@gmail.com>
2025-05-17 20:28:14 -0300

    rename user.py to usuario.py

--- sistema/backend/app/models/usuario.py
+++ sistema/backend/app/models/usuario.py
@@ -1,17 +1,17 @@
 from datetime import datetime, timezone, date
-from app.extensions import db
+from app.infra.extensions import db
 from werkzeug.security import check_password_hash
 from enum import Enum
 from sqlalchemy import Enum as SAEnum
 
 
-class UserTipoEnum(str, Enum):
+class UsuarioTipoEnum(str, Enum):
     Admin = "admin"
     Secretario = "secretario"
     Professor = "professor"
 
 
-class User(db.Model):
+class Usuario(db.Model):
 
     __tablename__ = "usuario"
 
@@ -20,7 +20,7 @@ class User(db.Model):
     ultimo_nome = db.Column(db.String(50), nullable=False)
     usuario = db.Column(db.String(255), nullable=False, unique=True)
     senha = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(60), nullable=True)
+    email = db.Column(db.String(60), nullable=False)
     data_nascimento = db.Column(db.Date, nullable=False)
     sexo = db.Column(db.String(1), nullable=False)
     cpf = db.Column(db.String(11), nullable=False, unique=True)
@@ -28,31 +28,15 @@ class User(db.Model):
 
     # 0 - admin, 1-secretario, 2-professor
     tipo = db.Column(
-        SAEnum(UserTipoEnum, name="user_tipo_enum", native_enum=False),
+        SAEnum(UsuarioTipoEnum, name="user_tipo_enum", native_enum=False),
         nullable=False,
-        default=UserTipoEnum.Secretario,
+        default=UsuarioTipoEnum.Secretario,
     )
 
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(
         db.DateTime, default=None, onupdate=lambda: datetime.now(timezone.utc)
     )
-
-    # id_user_insert = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable=False)
-    # inserted_by = db.relationship(
-    #     "User"
-    #     , remote_side=[id]
-    #     ,foreign_keys=[id_user_insert]
-    #     , backref="inserted_users"
-    # )
-
-    # id_user_update = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable=False)
-    # updated_by = db.relationship(
-    #     "User"
-    #     , remote_side=[id]
-    #     ,foreign_keys=[id_user_update]
-    #     , backref="updated_users"
-    # )
 
     def __repr__(self):
         return f"<Usuário {self.usuario}>"
